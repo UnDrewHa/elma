450e71b92401591aa77a1e12c95fc8d9a08c2f50
Gorokhov Andrey <a.gorokhov@rambler-co.ru>
2017-04-19 12:14:36 +0300

    buttons hover, product toggle

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,7 @@
 //= plugins.js
 $(document).ready(function() {
+
+	// sliders
 	$('[data-slick]').each(function () {
         var self = $(this);
         if(self.hasClass('slick-initialized')){
@@ -17,4 +19,25 @@ $(document).ready(function() {
 
         self.slick(slickConfig);
     });
+
+	// lightbox
+	$('.photo-slider__item').magnificPopup({
+	  type: 'image',
+	  gallery:{
+	    enabled:true,
+			tCounter : '',
+			tPrev : 'Предыдущая фотография',
+			tNext : 'Следующая фотография'
+	  },
+		closeMarkup : '<button type="button" class="mfp-close"></button>'
+	});
+
+	$('.products-button').on('click', 'li', function () {
+		var productNum = $(this).attr('data-product');
+
+		$('.products-item, .products-button li').removeClass('_active');
+
+		$(this).addClass('_active');
+		$('.products-item[data-product="' + productNum + '"]').addClass('_active');
+	});
 });
